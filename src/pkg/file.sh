2d927c2067927da897788cf54688db8b0f72b589
Arnau478 <arnauxabia@gmail.com>
2025-06-05 22:43:06 +0200

    Compile "file" statically (almost, see #3)

--- src/pkg/file.sh
+++ src/pkg/file.sh
@@ -5,7 +5,9 @@ build() {
     wget -nc https://astron.com/pub/file/file-$FILE_VERSION.tar.gz
     tar -xf file-$FILE_VERSION.tar.gz
     cd file-$FILE_VERSION
-    ./configure --prefix=$OUT/usr
+    mkdir out
+    ./configure --prefix=$(realpath out) LD="musl-gcc" CCFLAGS="-Os" LDFLAGS="-static" --disable-shared --enable-static
     make
     make install
+    mv out/bin/file $OUT/usr/bin/file
 }
