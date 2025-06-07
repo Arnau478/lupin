9db40e63e8053ca90a9f98dd32e88508a10fac7e
Arnau478 <arnauxabia@gmail.com>
2025-06-07 18:48:27 +0200

    Install `magic.mgc`

--- src/pkg/file.sh
+++ src/pkg/file.sh
@@ -6,8 +6,12 @@ build() {
     tar -xf file-$FILE_VERSION.tar.gz
     cd file-$FILE_VERSION
     mkdir out
-    ./configure --prefix=$(realpath out) CC="zig cc -target x86_64-linux-musl" CCFLAGS="-Os" LDFLAGS="-static" --disable-shared --enable-static
+    ./configure --prefix=/usr --datadir=/usr/share/file CC="zig cc -target x86_64-linux-musl" CCFLAGS="-Os" LDFLAGS="-static" --disable-shared --enable-static
     make
-    make install
-    mv out/bin/file $OUT/usr/bin/file
+    make DESTDIR=$(realpath out) install
+    cp out/usr/bin/file $OUT/usr/bin/file
+    cp out/usr/include/magic.h $OUT/usr/include/magic.h
+    cp -r out/usr/share/man/* $OUT/usr/share/man
+    mkdir -p $OUT/usr/share/file/misc
+    cp out/usr/share/file/misc/magic.mgc $OUT/usr/share/file/misc/magic.mgc
 }
